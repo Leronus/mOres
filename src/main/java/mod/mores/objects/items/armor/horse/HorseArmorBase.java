cb7b2b5c90710afb0a1bcc6d56c504843e7f793c
Leronus <leronus@live.nl>
2021-02-28 16:37:45 +0100

    Horse Armor no longer stackable (stackSize 1 instead of 64)

--- src/main/java/mod/mores/objects/items/armor/horse/HorseArmorBase.java
+++ src/main/java/mod/mores/objects/items/armor/horse/HorseArmorBase.java
@@ -3,29 +3,37 @@ package mod.mores.objects.items.armor.horse;
 import mod.mores.init.ItemInit;
 import mod.mores.modid.Mores;
 import mod.mores.util.Reference;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.passive.HorseArmorType;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
 import net.minecraftforge.common.util.EnumHelper;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
+import java.util.List;
+
 public class HorseArmorBase extends Item {
 
-    public String name;
+    private String name;
+    private int armorProtection;
 
     public HorseArmorBase(String name, int armorHardness){
         setRegistryName(name + "_horse_armor");
         setUnlocalizedName(name + "_horse_armor");
         setCreativeTab(Mores.MORESITEMSTAB);
+        setMaxStackSize(1);
+
+        this.name = name;
+        this.armorProtection = armorHardness;
 
         ItemInit.ITEMS.add(this);
         generateHorseArmorType(name, armorHardness);
     }
 
     public HorseArmorType generateHorseArmorType(String name, int armorHardness) {
-            this.name = name;
-
             final String textureLocation = Reference.MODID + ":textures/entity/horse/armor/horse_armor_" + name + ".png";
             final int armorStrength = armorHardness;
 
@@ -36,5 +44,18 @@ public class HorseArmorBase extends Item {
     public HorseArmorType getHorseArmorType(ItemStack stack){
 
         return HorseArmorType.getByName(name);
+    }
+
+    @Override
+    public Item setMaxStackSize(int maxStackSize) {
+        return super.setMaxStackSize(maxStackSize);
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+        //Making the tooltips
+        //tooltip.add("Text: " + TextFormatting.COLOR + toolMaterial.get);
+        tooltip.add("Max Stack Size: " + TextFormatting.LIGHT_PURPLE + maxStackSize);
+        tooltip.add("Protection: " + TextFormatting.GOLD + armorProtection);
     }
 }
