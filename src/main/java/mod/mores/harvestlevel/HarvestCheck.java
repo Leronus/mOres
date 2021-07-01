7e65bf7cd7a4a5c8d6875209e6aff13481bfc174
Leronus <leronus@live.nl>
2021-07-02 01:29:30 +0200

    Diamond & netherite block harvestlevel changes

--- src/main/java/mod/mores/harvestlevel/HarvestCheck.java
+++ src/main/java/mod/mores/harvestlevel/HarvestCheck.java
@@ -1,6 +1,11 @@
 package mod.mores.harvestlevel;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraftforge.common.ToolType;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -9,16 +14,29 @@ import net.minecraftforge.fml.common.Mod;
 public class HarvestCheck {
     @SubscribeEvent
     public static void doPlayerHarvestCheck(PlayerEvent.HarvestCheck event) {
+        Item mainhandItem = event.getPlayer().getMainHandItem().getItem();
+        Block targetBlock = event.getTargetBlock().getBlock();
         boolean success = false;
-        if (event.getPlayer().getMainHandItem().getItem() == Items.DIAMOND_PICKAXE) {
+
+        if (mainhandItem == Items.DIAMOND_PICKAXE) {
             if (event.getTargetBlock().getHarvestLevel() <= 5) {
                 success = true;
             }
-        } else if (event.getPlayer().getMainHandItem().getItem() == Items.NETHERITE_PICKAXE) {
+        } else if (mainhandItem == Items.NETHERITE_PICKAXE) {
             if (event.getTargetBlock().getHarvestLevel() <= 7) {
                 success = true;
             }
-            event.setCanHarvest(success);
+        } else if (targetBlock == Blocks.DIAMOND_BLOCK || targetBlock ==  Blocks.NETHERITE_BLOCK){
+            String mainhandItemName = String.valueOf(mainhandItem);
+            String[] tooltypeName = mainhandItemName.split("_");
+            ToolType toolType = ToolType.get(tooltypeName[0]);
+            //Diamond & netherite block should be minable with harvest level 5 or higher
+            if (event.getPlayer().getMainHandItem().getHarvestLevel(toolType, event.getPlayer(), event.getTargetBlock().getBlockState()) >= 5){
+                success = true;
+            } else{
+                success = false;
+            }
         }
+        event.setCanHarvest(success);
     }
 }
