6f3e570473629ff5bac2318952db8c4001480369
Leronus <leronus@live.nl>
2021-07-08 02:41:29 +0200

    Revert "Rever2"
    This reverts commit ebea552e

---
+++ src/main/java/mod/mores/events/PlayerEventsServer.java
@@ -0,0 +1,32 @@
+package mod.mores.events;
+
+import mod.mores.Mores;
+import mod.mores.init.BlockInit;
+import mod.mores.init.SoundTypeInit;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.event.entity.item.ItemTossEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+//@Mod.EventBusSubscriber(modid = Mores.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class PlayerEventsServer {
+//
+//    @OnlyIn(Dist.DEDICATED_SERVER)
+//    @SubscribeEvent
+//    public static void onPlayerToss(ItemTossEvent itemTossEvent){
+//        PlayerEntity player = itemTossEvent.getPlayer();
+//        World world = player.getCommandSenderWorld();
+//        BlockState state = world.getBlockState(player.blockPosition().below());
+//
+//        if (state.getBlock() == BlockInit.TURQUOISE_BLOCK.get()){
+//            Mores.LOGGER.info("Player tossed on turquoise block!");
+//            //Client
+//            world.playSound(null, player.blockPosition(), SoundTypeInit.DARUDE_SANDSTORM.get(), SoundCategory.MUSIC, 1, 1);
+//        }
+//    }
+}
