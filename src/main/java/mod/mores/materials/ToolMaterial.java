5a36e6056c9feadeffa7e75b264e82db8d157118
Leronus <leronus@live.nl>
2021-08-24 02:22:19 +0200

    Fixed crash because of using tags in Ingredients

--- src/main/java/mod/mores/materials/ToolMaterial.java
+++ src/main/java/mod/mores/materials/ToolMaterial.java
@@ -14,8 +14,8 @@ import java.util.function.Supplier;
  * @author Leronus
  */
 public enum ToolMaterial implements Tier {
-    WOOD(0, 59, 2.0F, 0.0F, 15, () -> Ingredient.of(ItemTags.PLANKS)),
-    STONE(1, 131, 4.0F, 1.0F, 5, () -> Ingredient.of(ItemTags.STONE_TOOL_MATERIALS)),
+    WOOD(0, 59, 2.0F, 0.0F, 15, () -> Ingredient.of(Items.OAK_PLANKS)),
+    STONE(1, 131, 4.0F, 1.0F, 5, () -> Ingredient.of(Items.STONE)),
     IRON(2, 250, 6.0F, 2.0F, 14, () -> Ingredient.of(Items.IRON_INGOT)),
     DIAMOND(3, 1561, 8.0F, 3.0F, 10, () -> Ingredient.of(Items.DIAMOND)),
     GOLD(0, 32, 12.0F, 0.0F, 22, () -> Ingredient.of(Items.GOLD_INGOT)),
