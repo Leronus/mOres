f566f6b22bc8ca7ada79045a86791a27a900386e
Leronus <leronus@live.nl>
2021-08-24 02:21:40 +0200

    Updated oregen to 1.17

--- src/main/java/mod/mores/world/OreGeneration.java
+++ src/main/java/mod/mores/world/OreGeneration.java
@@ -1,29 +1,88 @@
-//package mod.mores.world;
-//
-//import mod.mores.init.BlockInit;
-//import net.minecraft.world.level.biome.Biome;
-//import net.minecraft.world.level.block.Blocks;
-//import net.minecraft.world.level.block.state.BlockState;
-//import net.minecraft.world.level.levelgen.feature.Feature;
-//import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
-//import net.minecraft.world.level.levelgen.placement.RangeDecorator;
-//import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTest;
-//import net.minecraftforge.common.world.BiomeGenerationSettingsBuilder;
-//import net.minecraftforge.event.world.BiomeLoadingEvent;
-//
-///**
-// * This class handles the ore generation for all ores added by mOres
-// * @author Leronus
-// */
-//public class OreGeneration {
-////    Create new fillerBlockType
-//    public static final RuleTest END_STONE = new BlockMatchRuleTest(Blocks.END_STONE);
-//
-//    /**
-//     * Generates all the ores in the three different world biomes
-//     * @param event Type of world biome
-//     */
-//    public static void generateOres(final BiomeLoadingEvent event) {
+package mod.mores.world;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+import mod.mores.init.BlockInit;
+import net.minecraft.core.Registry;
+import net.minecraft.data.BuiltinRegistries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.BiomeGenerationSettings;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraft.world.level.levelgen.VerticalAnchor;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
+import net.minecraft.world.level.levelgen.placement.RangeDecorator;
+import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTest;
+import net.minecraftforge.common.world.BiomeGenerationSettingsBuilder;
+import net.minecraftforge.event.world.BiomeLoadingEvent;
+import net.minecraftforge.fml.util.ObfuscationReflectionHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
+
+/**
+ * This class handles the ore generation for all ores added by mOres
+ * @author Leronus
+ */
+public class OreGeneration {
+    public static void initOres() {
+		/*
+		ORE_GOLD = register("ore_gold",
+		Feature.ORE.configured(new OreConfiguration(ORE_GOLD_TARGET_LIST, 9))
+		.rangeUniform(VerticalAnchor.bottom(), VerticalAnchor.absolute(31)).squared().count(2));
+		ORE_GOLD_TARGET_LIST = ImmutableList.of(
+		OreConfiguration.target(OreConfiguration.Predicates.STONE_ORE_REPLACEABLES, Features.States.GOLD_ORE),
+		OreConfiguration.target(OreConfiguration.Predicates.DEEPSLATE_ORE_REPLACEABLES, Features.States.DEEPSLATE_GOLD_ORE));
+		 */
+        ImmutableList<OreConfiguration.TargetBlockState> SAPPHIRE_ORE_TARGETS = ImmutableList.of(
+                OreConfiguration.target(OreConfiguration.Predicates.STONE_ORE_REPLACEABLES, BlockInit.SAPPHIRE_ORE.get().defaultBlockState()),
+                // TODO add Deepslate ore variant
+                OreConfiguration.target(OreConfiguration.Predicates.DEEPSLATE_ORE_REPLACEABLES, BlockInit.DEEPSLATE_SAPPHIRE_ORE.get().defaultBlockState())
+
+        );
+        ImmutableList<OreConfiguration.TargetBlockState> NETHER_SILVER_ORE_TARGETS = ImmutableList.of(
+                OreConfiguration.target(OreConfiguration.Predicates.NETHER_ORE_REPLACEABLES, BlockInit.NETHER_SILVER_ORE.get().defaultBlockState()));
+
+        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, BlockInit.NETHER_SILVER_ORE.get().getRegistryName(),
+                Feature.ORE
+                        .configured(new OreConfiguration(NETHER_SILVER_ORE_TARGETS, 10 /*veinSize*/))
+                        .rangeUniform(
+                                VerticalAnchor.absolute(0 /*minY*/),
+                                VerticalAnchor.absolute(100 /*maxY*/))
+                        .squared()
+                        .count(35 /*veinCount*/));
+    }
+
+    public static void setupOres() {
+        for (Map.Entry<ResourceKey<Biome>, Biome> biome : BuiltinRegistries.BIOME.entrySet()) {
+            if (!biome.getValue().getBiomeCategory().equals(Biome.BiomeCategory.NETHER)
+                    && !biome.getValue().getBiomeCategory().equals(Biome.BiomeCategory.THEEND)) {
+                addFeatureToBiome(biome.getValue(), GenerationStep.Decoration.UNDERGROUND_ORES,
+                        BuiltinRegistries.CONFIGURED_FEATURE.get(BlockInit.NETHER_SILVER_ORE.get().getRegistryName()));
+            }
+        }
+    }
+
+    public static void addFeatureToBiome(Biome biome, GenerationStep.Decoration decoration,
+                                         ConfiguredFeature<?, ?> configuredFeature) {
+        List<List<Supplier<ConfiguredFeature<?, ?>>>> biomeFeatures = new ArrayList<>(
+                biome.getGenerationSettings().features());
+        while (biomeFeatures.size() <= decoration.ordinal()) {
+            biomeFeatures.add(Lists.newArrayList());
+        }
+        List<Supplier<ConfiguredFeature<?, ?>>> features = new ArrayList<>(biomeFeatures.get(decoration.ordinal()));
+        features.add(() -> configuredFeature);
+        biomeFeatures.set(decoration.ordinal(), features);
+
+        ObfuscationReflectionHelper.setPrivateValue(BiomeGenerationSettings.class, biome.getGenerationSettings(), biomeFeatures,
+                "features");
+    }
 //        //Overworld
 //        if (!(event.getCategory().equals(Biome.BiomeCategory.NETHER) || event.getCategory().equals(Biome.BiomeCategory.THEEND))) {
 //            generateOre(event.getGeneration(), OreConfiguration.Predicates.NATURAL_STONE,
@@ -79,5 +138,5 @@
 //                Feature.ORE.configured(new OreConfiguration(fillerType, state, veinSize))
 //                        .decorated(RangeDecorator.RANGE.configured(new TopSolidRangeConfig(minHeight, 0, maxHeight)))
 //                        .squared().count(amount));
-//    }
-//}
+}
+
