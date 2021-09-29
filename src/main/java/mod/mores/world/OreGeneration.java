912e556130ea4dc9dc8b049bc5a509628591b454
Leronus <leronus@live.nl>
2021-09-29 21:38:16 +0200

    OreGeneration tweaks, added Deepslate block

--- src/main/java/mod/mores/world/OreGeneration.java
+++ src/main/java/mod/mores/world/OreGeneration.java
@@ -1,6 +1,8 @@
 package mod.mores.world;
 
 import mod.mores.init.BlockInit;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.world.biome.Biome;
@@ -21,6 +23,9 @@ import net.minecraftforge.event.world.BiomeLoadingEvent;
 public class OreGeneration {
     //Create new fillerBlockType
     public static final RuleTest END_STONE = new BlockMatchRuleTest(Blocks.END_STONE);
+    public static final RuleTest NATURAL_STONE = OreFeatureConfig.FillerBlockType.NATURAL_STONE;
+    public static final RuleTest NETHERRACK = OreFeatureConfig.FillerBlockType.NETHERRACK;
+//    public static final RuleTest DEEPSLATE_BLOCK = new BlockMatchRuleTest(BlockInit.DEEPSLATE_BLOCK.get());
 
     /**
      * Generates all the ores in the three different world biomes
@@ -29,34 +34,34 @@ public class OreGeneration {
     public static void generateOres(final BiomeLoadingEvent event) {
         //Overworld
         if (!(event.getCategory().equals(Biome.Category.NETHER) || event.getCategory().equals(Biome.Category.THEEND))) {
-            generateOre(event.getGeneration(), OreFeatureConfig.FillerBlockType.NATURAL_STONE,
-                    BlockInit.TIN_ORE.get().defaultBlockState(), 11, 50, 80, 34);
-            generateOre(event.getGeneration(), OreFeatureConfig.FillerBlockType.NATURAL_STONE,
-                    BlockInit.COPPER_ORE.get().defaultBlockState(), 10, 45, 75, 32);
-            generateOre(event.getGeneration(), OreFeatureConfig.FillerBlockType.NATURAL_STONE,
-                    BlockInit.SILVER_ORE.get().defaultBlockState(), 7, 31, 58, 16);
-            generateOre(event.getGeneration(), OreFeatureConfig.FillerBlockType.NATURAL_STONE,
-                    BlockInit.COBALT_ORE.get().defaultBlockState(), 6, 22, 39, 17);
-            generateOre(event.getGeneration(), OreFeatureConfig.FillerBlockType.NATURAL_STONE,
-                    BlockInit.AMETHYST_ORE.get().defaultBlockState(), 7, 17, 31, 15);
-            generateOre(event.getGeneration(), OreFeatureConfig.FillerBlockType.NATURAL_STONE,
-                    BlockInit.TOPAZ_ORE.get().defaultBlockState(), 5, 10, 21, 10);
-            generateOre(event.getGeneration(), OreFeatureConfig.FillerBlockType.NATURAL_STONE,
-                    BlockInit.TOURMALINE_ORE.get().defaultBlockState(), 5, 11, 23, 12);
-            generateOre(event.getGeneration(), OreFeatureConfig.FillerBlockType.NATURAL_STONE,
+            generateOre(event.getGeneration(), NATURAL_STONE,
+                    BlockInit.TIN_ORE.get().defaultBlockState(), 11, 54, 145, 34);
+            generateOre(event.getGeneration(), NATURAL_STONE,
+                    BlockInit.COPPER_ORE.get().defaultBlockState(), 10, 47, 130, 32);
+            generateOre(event.getGeneration(), NATURAL_STONE,
+                    BlockInit.SILVER_ORE.get().defaultBlockState(), 7, 31, 65, 18);
+            generateOre(event.getGeneration(), NATURAL_STONE,
+                    BlockInit.COBALT_ORE.get().defaultBlockState(), 6, 22, 42, 17);
+            generateOre(event.getGeneration(), NATURAL_STONE,
+                    BlockInit.AMETHYST_ORE.get().defaultBlockState(), 7, 17, 34, 15);
+            generateOre(event.getGeneration(), NATURAL_STONE,
+                    BlockInit.TOPAZ_ORE.get().defaultBlockState(), 5, 10, 21, 9);
+            generateOre(event.getGeneration(), NATURAL_STONE,
+                    BlockInit.TOURMALINE_ORE.get().defaultBlockState(), 5, 11, 23, 11);
+            generateOre(event.getGeneration(), NATURAL_STONE,
                     BlockInit.SAPPHIRE_ORE.get().defaultBlockState(), 4, 4, 15, 4);
-            generateOre(event.getGeneration(), OreFeatureConfig.FillerBlockType.NATURAL_STONE,
+            generateOre(event.getGeneration(), NATURAL_STONE,
                     BlockInit.RUBY_ORE.get().defaultBlockState(), 4, 6, 14, 6);
-            generateOre(event.getGeneration(), OreFeatureConfig.FillerBlockType.NATURAL_STONE,
+            generateOre(event.getGeneration(), NATURAL_STONE,
                     BlockInit.TURQUOISE_ORE.get().defaultBlockState(), 3, 0, 10, 5);
         }
         //Nether
         else if (event.getCategory().equals(Biome.Category.NETHER)){
-            generateOre(event.getGeneration(), OreFeatureConfig.FillerBlockType.NETHERRACK,
+            generateOre(event.getGeneration(), NETHERRACK,
                     BlockInit.NETHER_SILVER_ORE.get().defaultBlockState(), 10, 0, 100, 35);
-            generateOre(event.getGeneration(), OreFeatureConfig.FillerBlockType.NETHERRACK,
+            generateOre(event.getGeneration(), NETHERRACK,
                     BlockInit.MOISSANITE_NETHER_ORE.get().defaultBlockState(), 4, 0, 50, 16);
-            generateOre(event.getGeneration(), OreFeatureConfig.FillerBlockType.NETHERRACK,
+            generateOre(event.getGeneration(), NETHERRACK,
                     BlockInit.TANZANITE_ORE.get().defaultBlockState(), 5, 15, 70, 25);
         }
         //End
