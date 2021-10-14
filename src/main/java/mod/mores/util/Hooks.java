de36276c48f75eba0fc0154a3348a560e01a176e
Leronus <leronus@live.nl>
2021-10-14 16:38:51 +0200

    Added hooks, TooltipHandler

---
+++ src/main/java/mod/mores/util/Hooks.java
@@ -0,0 +1,55 @@
+package mod.mores.util;
+
+import mod.mores.config.Config;
+import mod.mores.objects.ItemShield;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.item.ShieldItem;
+import net.minecraft.util.DamageSource;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
+
+public class Hooks {
+
+//    public static float getDamageReduction(LivingEntity victim, DamageSource source, float damage) {
+//        float f1 = 0.0f;
+//        if (damage > 0.0F && victim.isDamageSourceBlocked(source)) {
+//            victim.hurtCurrentlyUsedShield(damage);
+//            f1 = damage;
+//            float reduction = 1f;
+//            //getUseItem was getActiveItemStack
+//            Item shield = victim.getUseItem().getItem();
+//            if (shield instanceof ItemShield) {
+//                reduction = ((ItemShield) shield).getDamageReduction() / 100f;
+//            } else if (shield == Items.SHIELD
+//                    || (!Config.customShieldMaxReduction.get() && victim.getUseItem().isShield(victim))) {
+//                reduction = Config.defaultDamageReduction.get() / 100f;
+//            }
+//            if (!source.isProjectile() && reduction < 1f)
+//                f1 = damage * reduction;
+//            if (!source.isProjectile()) {
+//                Entity entity = source.getDirectEntity();
+//                if (entity instanceof LivingEntity) {
+//                    victim.blockUsingShield((LivingEntity) entity);
+//                }
+//            }
+//        }
+//        return f1;
+//    }
+
+    public static boolean canThornsApply(ItemStack stack) {
+        if (Config.thornsOnShields.get()) {
+            return stack.getItem() instanceof ArmorItem || stack.getItem() instanceof ShieldItem;
+        } else {
+            return stack.getItem() instanceof ArmorItem;
+        }
+    }
+
+    public static boolean damageEntityFromReturn(DamageSource source, boolean flag) {
+        return flag || !source.isProjectile();
+    }
+
+}
--- src/main/java/mod/mores/util/TooltipHandler.java
+++ src/main/java/mod/mores/util/TooltipHandler.java
@@ -15,25 +15,25 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
 import java.util.List;
 
-//@EventBusSubscriber(value = Dist.CLIENT, modid = Mores.MODID)
-//public class TooltipHandler {
-//
-//    @SubscribeEvent
-//    public static void onTooltip(ItemTooltipEvent e) {
-//        if (e.getItemStack().getItem() instanceof ItemShield) {
-//            Item shield = e.getItemStack().getItem();
-//            List<ITextComponent> tooltip = e.getToolTip();
-//            tooltip.add(new StringTextComponent(""));
-//            tooltip.add(Mores.getBlockingTextComponent());
-//            if (shield == Items.SHIELD) {
-//                tooltip.add(Mores.getDamageReductionTextComponent(Config.defaultDamageReduction.get()));
-//            } else if (shield instanceof ItemShield) {
-//                tooltip.add(Mores
-//                        .getDamageReductionTextComponent(((ItemShield) shield).getDamageReduction()));
-//            } else {
-//                tooltip.add(Mores.getDamageReductionTextComponent(
-//                        Config.customShieldMaxReduction.get() ? 100 : Config.defaultDamageReduction.get()));
-//            }
-//        }
-//    }
-//}
+@EventBusSubscriber(value = Dist.CLIENT, modid = Mores.MODID)
+public class TooltipHandler {
+
+    @SubscribeEvent
+    public static void onTooltip(ItemTooltipEvent e) {
+        if (e.getItemStack().getItem() instanceof ItemShield) {
+            Item shield = e.getItemStack().getItem();
+            List<ITextComponent> tooltip = e.getToolTip();
+            tooltip.add(new StringTextComponent(""));
+            tooltip.add(ItemShield.getBlockingTextComponent());
+            if (shield == Items.SHIELD) {
+                tooltip.add(ItemShield.getDamageReductionTextComponent(Config.defaultDamageReduction.get()));
+            } else if (shield instanceof ItemShield) {
+                tooltip.add(ItemShield
+                        .getDamageReductionTextComponent(((ItemShield) shield).getDamageReduction()));
+            } else {
+                tooltip.add(ItemShield.getDamageReductionTextComponent(
+                        Config.customShieldMaxReduction.get() ? 100 : Config.defaultDamageReduction.get()));
+            }
+        }
+    }
+}
