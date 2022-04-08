9257b4aa14f63870f8e335a65b1698bcb693ed24
Leronus <leronus@live.nl>
2022-04-08 23:06:55 +0200

    Cleaned up code in ConfiguredFeatures

--- src/main/java/mod/mores/core/world/feature/ModConfiguredFeatures.java
+++ src/main/java/mod/mores/core/world/feature/ModConfiguredFeatures.java
@@ -4,79 +4,60 @@ import mod.mores.core.init.BlockInit;
 import net.minecraft.core.Holder;
 import net.minecraft.data.worldgen.features.FeatureUtils;
 import net.minecraft.data.worldgen.features.OreFeatures;
-import net.minecraft.data.worldgen.features.TreeFeatures;
-import net.minecraft.data.worldgen.placement.PlacementUtils;
-import net.minecraft.util.valueproviders.ConstantInt;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
-import net.minecraft.world.level.levelgen.feature.WeightedPlacedFeature;
 import net.minecraft.world.level.levelgen.feature.configurations.*;
-import net.minecraft.world.level.levelgen.feature.featuresize.TwoLayersFeatureSize;
-import net.minecraft.world.level.levelgen.feature.foliageplacers.BlobFoliagePlacer;
-import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
-import net.minecraft.world.level.levelgen.feature.trunkplacers.StraightTrunkPlacer;
-import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import net.minecraft.world.level.levelgen.structure.templatesystem.BlockMatchTest;
 import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTest;
-import net.minecraft.world.level.levelgen.structure.templatesystem.TagMatchTest;
 
 import java.util.List;
 
 public class ModConfiguredFeatures {
+
     public static final RuleTest END_STONE_REPLACEABLES = new BlockMatchTest(Blocks.END_STONE);
+
 
     public static final List<OreConfiguration.TargetBlockState> OVERWORLD_TIN_ORES = List.of(
             OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES, BlockInit.TIN_ORE.get().defaultBlockState()),
             OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES, BlockInit.DEEPSLATE_TIN_ORE.get().defaultBlockState()));
-
     public static final List<OreConfiguration.TargetBlockState> OVERWORLD_COBALT_ORES = List.of(
             OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES, BlockInit.COBALT_ORE.get().defaultBlockState()),
             OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES, BlockInit.DEEPSLATE_COBALT_ORE.get().defaultBlockState()));
-
     public static final List<OreConfiguration.TargetBlockState> OVERWORLD_SILVER_ORES = List.of(
             OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES, BlockInit.SILVER_ORE.get().defaultBlockState()),
             OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES, BlockInit.DEEPSLATE_SILVER_ORE.get().defaultBlockState()));
-
-    public static final List<OreConfiguration.TargetBlockState> NETHER_SILVER_ORES = List.of(
-            OreConfiguration.target(OreFeatures.NETHER_ORE_REPLACEABLES, BlockInit.NETHER_SILVER_ORE.get().defaultBlockState()));
-
     public static final List<OreConfiguration.TargetBlockState> OVERWORLD_TOPAZ_ORES = List.of(
             OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES, BlockInit.TOPAZ_ORE.get().defaultBlockState()),
             OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES, BlockInit.DEEPSLATE_TOPAZ_ORE.get().defaultBlockState()));
-
     public static final List<OreConfiguration.TargetBlockState> OVERWORLD_TOURMALINE_ORES = List.of(
             OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES, BlockInit.TOURMALINE_ORE.get().defaultBlockState()),
             OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES, BlockInit.DEEPSLATE_TOURMALINE_ORE.get().defaultBlockState()));
-
-    public static final List<OreConfiguration.TargetBlockState> NETHER_TANZANITE_ORES = List.of(
-            OreConfiguration.target(OreFeatures.NETHER_ORE_REPLACEABLES, BlockInit.TANZANITE_ORE.get().defaultBlockState())
-//            ,OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES, BlockInit.DEEPSLATE_TANZANITE_ORE.get().defaultBlockState())
-            );
-
     public static final List<OreConfiguration.TargetBlockState> OVERWORLD_SAPPHIRE_ORES = List.of(
             OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES, BlockInit.SAPPHIRE_ORE.get().defaultBlockState()),
             OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES, BlockInit.DEEPSLATE_SAPPHIRE_ORE.get().defaultBlockState()));
-
     public static final List<OreConfiguration.TargetBlockState> OVERWORLD_RUBY_ORES = List.of(
             OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES, BlockInit.RUBY_ORE.get().defaultBlockState()),
             OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES, BlockInit.DEEPSLATE_RUBY_ORE.get().defaultBlockState()));
-
     public static final List<OreConfiguration.TargetBlockState> OVERWORLD_MOISSANITE_ORES = List.of(
             OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES, BlockInit.MOISSANITE_ORE.get().defaultBlockState()),
             OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES, BlockInit.DEEPSLATE_MOISSANITE_ORE.get().defaultBlockState()));
-
     public static final List<OreConfiguration.TargetBlockState> OVERWORLD_TURQUOISE_ORES = List.of(
             OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES, BlockInit.TURQUOISE_ORE.get().defaultBlockState()),
             OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES, BlockInit.DEEPSLATE_TURQUOISE_ORE.get().defaultBlockState()));
 
+    public static final List<OreConfiguration.TargetBlockState> NETHER_SILVER_ORES = List.of(
+            OreConfiguration.target(OreFeatures.NETHER_ORE_REPLACEABLES, BlockInit.NETHER_SILVER_ORE.get().defaultBlockState()));
+    public static final List<OreConfiguration.TargetBlockState> NETHER_TANZANITE_ORES = List.of(
+            OreConfiguration.target(OreFeatures.NETHER_ORE_REPLACEABLES, BlockInit.TANZANITE_ORE.get().defaultBlockState())
+           /*,OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES, BlockInit.DEEPSLATE_TANZANITE_ORE.get().defaultBlockState())*/);
+    public static final List<OreConfiguration.TargetBlockState> NETHER_MOISSANITE_ORES = List.of(
+            OreConfiguration.target(OreFeatures.NETHER_ORE_REPLACEABLES, BlockInit.MOISSANITE_NETHER_ORE.get().defaultBlockState()));
+
     public static final List<OreConfiguration.TargetBlockState> END_ONYX_ORES = List.of(
             OreConfiguration.target(END_STONE_REPLACEABLES, BlockInit.ONYX_ORE.get().defaultBlockState()));
 
-        public static final List<OreConfiguration.TargetBlockState> NETHER_MOISSANITE_ORES = List.of(
-            OreConfiguration.target(OreFeatures.NETHER_ORE_REPLACEABLES, BlockInit.MOISSANITE_NETHER_ORE.get().defaultBlockState()));
-
-
+    /* -------------------------------------------------------------------------------------------------------------------------------------------------------- */
 
     public static final Holder<ConfiguredFeature<OreConfiguration, ?>> TIN_ORE = FeatureUtils.register("tin_ore",
             Feature.ORE, new OreConfiguration(OVERWORLD_TIN_ORES, 12)); /* pSize not below 2/3 */
@@ -145,6 +126,4 @@ public class ModConfiguredFeatures {
 
     public static final Holder<ConfiguredFeature<OreConfiguration, ?>> NETHER_MOISSANITE_ORE = FeatureUtils.register("nether_moissanite_ore",
             Feature.ORE, new OreConfiguration(NETHER_MOISSANITE_ORES, 9));
-
-
 }
