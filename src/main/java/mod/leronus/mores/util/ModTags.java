aa5549afd6d10986f8c86efb21dde73535bd67a5
Leronus <leroy.de.smet@hva.nl>
2023-03-13 03:22:17 +0100

    Added mOres images from 1.16.5 as well as the new images for 1.19.2
    - Fixed nuggets not having a forge tag

--- src/main/java/mod/leronus/mores/util/ModTags.java
+++ src/main/java/mod/leronus/mores/util/ModTags.java
@@ -32,6 +32,13 @@ public class ModTags {
         public static final TagKey<Item> STERLING_INGOTS = createForge("ingots/sterling_silver");
         public static final TagKey<Item> OBSIDIAN_INGOTS = createForge("ingots/obsidian");
 
+        public static final TagKey<Item> TIN_NUGGETS = createForge("nuggets/tin");
+        public static final TagKey<Item> SILVER_NUGGETS = createForge("nuggets/silver");
+        public static final TagKey<Item> BRONZE_NUGGETS = createForge("nuggets/bronze");
+        public static final TagKey<Item> COBALT_NUGGETS = createForge("nuggets/cobalt");
+        public static final TagKey<Item> STEEL_NUGGETS = createForge("nuggets/steel");
+        public static final TagKey<Item> STERLING_NUGGETS = createForge("nuggets/sterling_silver");
+
         public static final TagKey<Item> TOPAZ_GEMS = createForge("gems/topaz");
         public static final TagKey<Item> TOURMALINE_GEMS = createForge("gems/tourmaline");
         public static final TagKey<Item> RUBY_GEMS = createForge("gems/ruby");
