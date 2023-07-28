d50fc301a9382873263ac9b18fb42a5a6f69347a
Leronus <leroy.de.smet@hva.nl>
2023-07-28 04:42:42 +0200

    Fixed mixins and updated shields

---
+++ src/main/java/mod/leronus/mores/api/client/ClientMaterials.java
@@ -0,0 +1,6 @@
+package mod.leronus.mores.api.client;
+
+import net.minecraft.client.resources.model.Material;
+
+public record ClientMaterials(Material noPatternMaterial, Material patternMaterial) {
+}
--- src/main/java/mod/leronus/mores/item/client/ShieldBlockEntityRenderer.java
+++ src/main/java/mod/leronus/mores/item/client/ShieldBlockEntityRenderer.java
@@ -9,14 +9,12 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.geom.EntityModelSet;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.Sheets;
 import net.minecraft.client.renderer.blockentity.BannerRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderDispatcher;
 import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.resources.model.Material;
 import net.minecraft.client.resources.model.ModelBakery;
 import net.minecraft.core.Holder;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.block.entity.BannerBlockEntity;
 import net.minecraft.world.level.block.entity.BannerPattern;
@@ -24,7 +22,6 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterClientReloadListenersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
@@ -44,23 +41,22 @@ public class ShieldBlockEntityRenderer extends BlockEntityWithoutLevelRenderer {
     }
 
     @Override
-    public void renderByItem(@NotNull ItemStack itemStack, ItemDisplayContext itemDisplayContext, PoseStack poseStack, @NotNull MultiBufferSource multiBufferSource, int p_108834_, int p_108835_) {
+    public void renderByItem(ItemStack itemStack, ItemDisplayContext itemDisplayContext, PoseStack poseStack, MultiBufferSource bufferSource, int p_108834_, int p_108835_) {
         boolean hasBanner = BlockItem.getBlockEntityData(itemStack) != null;
         poseStack.pushPose();
         poseStack.scale(1.0F, -1.0F, -1.0F);
         Material material = hasBanner ? ModelBakery.SHIELD_BASE : ModelBakery.NO_PATTERN_SHIELD;
         if (itemStack.getItem() instanceof ModShieldItem shieldItem) {
-            material = hasBanner ? new Material(Sheets.SHIELD_SHEET, new ResourceLocation(Mores.MODID, "entity/shield/%s_shield".formatted(shieldItem.material.materialName))) : new Material(Sheets.SHIELD_SHEET, new ResourceLocation(Mores.MODID, "entity/shield/%s_shield_nopattern".formatted(shieldItem.material.materialName)));
+            material = shieldItem.getClientMaterial(hasBanner);
         }
-        VertexConsumer vertexconsumer = material.sprite().wrap(ItemRenderer.getFoilBufferDirect(multiBufferSource, this.shieldModel.renderType(material.atlasLocation()), true, itemStack.hasFoil()));
+        VertexConsumer vertexconsumer = material.sprite().wrap(ItemRenderer.getFoilBufferDirect(bufferSource, this.shieldModel.renderType(material.atlasLocation()), true, itemStack.hasFoil()));
         this.shieldModel.handle().render(poseStack, vertexconsumer, p_108834_, p_108835_, 1.0F, 1.0F, 1.0F, 1.0F);
         if (hasBanner) {
             List<Pair<Holder<BannerPattern>, DyeColor>> list = BannerBlockEntity.createPatterns(ShieldItem.getColor(itemStack), BannerBlockEntity.getItemPatterns(itemStack));
-            BannerRenderer.renderPatterns(poseStack, multiBufferSource, p_108834_, p_108835_, this.shieldModel.plate(), material, false, list, itemStack.hasFoil());
+            BannerRenderer.renderPatterns(poseStack, bufferSource, p_108834_, p_108835_, this.shieldModel.plate(), material, false, list, itemStack.hasFoil());
         } else {
             this.shieldModel.plate().render(poseStack, vertexconsumer, p_108834_, p_108835_, 1.0F, 1.0F, 1.0F, 1.0F);
         }
-
         poseStack.popPose();
     }
 }
