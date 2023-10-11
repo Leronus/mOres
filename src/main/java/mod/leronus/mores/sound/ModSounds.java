1bd95cf10a72af9ad70f0f1a667826c6436cb9e8
Leronus <leroy.de.smet@hva.nl>
2023-10-11 18:13:31 +0200

    - Fixed music discs not working - Fixed spawn rate of mOres music discs in dungeons - Updated forge

--- src/main/java/mod/leronus/mores/sound/ModSounds.java
+++ src/main/java/mod/leronus/mores/sound/ModSounds.java
@@ -11,9 +11,8 @@ import net.minecraftforge.registries.RegistryObject;
 public class ModSounds {
     public static final DeferredRegister<SoundEvent> SOUND_EVENTS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, Mores.MODID);
 
-    private static RegistryObject<SoundEvent> registerSoundEvent(String name){
-        ResourceLocation id = new ResourceLocation(Mores.MODID, name);
-        return SOUND_EVENTS.register(name, ()-> SoundEvent.createVariableRangeEvent(id));
+    private static RegistryObject<SoundEvent> registerSoundEvent(String name) {
+        return SOUND_EVENTS.register(name, () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(Mores.MODID, name)));
     }
 
     public static final RegistryObject<SoundEvent> LIVING_DUCK_SOUND = registerSoundEvent( "living_duck_sound");
